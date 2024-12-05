a07b68ed79811d4011b692c89621c48c08f7c530
D Q R <852601818@qq.com>
2024-12-05 15:43:39 +0800

    格式化配置

--- view/.prettierrc.js
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: dqr
- * @Date: 2024-12-05 11:31:15
- * @LastEditors: D Q R 852601818@qq.com
- * @LastEditTime: 2024-12-05 14:47:26
- * @FilePath: /hrsass-admin/view/.prettierrc.js
- * @Description: 
- * 
- */
